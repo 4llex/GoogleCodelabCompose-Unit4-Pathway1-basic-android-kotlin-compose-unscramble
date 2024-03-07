93362fa41fe5d2d582a13e1cab3bccf511d049f1
Alex Rosa <alexr@gec.inatel.br>
2024-03-07 17:39:17 -0300

    Write Unit Tests for ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    testImplementation("junit:junit:4.13.2")
 }
